35d45e9fbd27a0c292a4396735d9975bb6116dc2
kiranerella <kittu.nerella@gmail.com>
2025-07-08 12:04:04 -0400

    added sorta dynamic cost and region logic for MVP

--- codegen-api/main.py
+++ codegen-api/main.py
@@ -3,14 +3,13 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from utils import generate_main_tf
 import json
-# optionally: from utils import commit_and_push
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
-def form_get(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+# Load once at startup:
+pricing = json.load(open("pricing.json"))
+regions = json.load(open("regions.json"))
 
 @app.post("/generate", response_class=HTMLResponse)
 def form_post(
@@ -22,13 +21,13 @@ def form_post(
     db_identifier: str = Form(...)
 ):
     services = []
+
     if service_type == "container_orchestrator":
         services.append({
             "type": "container_orchestrator",
             "params": { "cluster_name": cluster_name }
         })
     elif service_type == "managed_postgres_db":
-        params = {}
         if cloud == "aws":
             params = {
                 "db_identifier": db_identifier,
@@ -39,7 +38,7 @@ def form_post(
         elif cloud == "azure":
             params = {
                 "db_name": db_identifier,
-                "resource_group_name": "my-resource-group",  # Could be dynamic
+                "resource_group_name": "my-resource-group",
                 "location": "eastus",
                 "admin_username": "admin",
                 "admin_password": "secret",
@@ -51,36 +50,36 @@ def form_post(
             }
         elif cloud == "gcp":
             params = {
-                "project_id": "my-gcp-project",  # Could be dynamic
+                "project_id": "my-gcp-project",
                 "db_name": db_identifier,
                 "region": "us-central1",
                 "tier": "db-custom-1-3840",
                 "admin_password": "secret",
                 "tags": {"Project": project_name}
             }
+        services.append({
+            "type": "managed_postgres_db",
+            "params": params
+        })
 
-    services.append({
-        "type": "managed_postgres_db",
-        "params": params
-    })
+    # dynamic cost & region logic block (should add api block for realtime processing ) ***
+    est_cost = sum(
+        pricing[cloud].get(s["type"], 0)
+        for s in services
+    )
+    default_region = regions[cloud][0]  # e.g., "us-east-1"
 
-
+    # Generate Terraform code
     user_input = {
         "cloud": cloud,
         "project_name": project_name,
         "services": services
     }
-
     tf_code = generate_main_tf(user_input)
 
-    # Optionally:
-    # commit_and_push(os.getcwd(), "infra", f"feat: add {service_type} on {cloud}")
-
-    return templates.TemplateResponse("result.html", {"request": request, "tf_code": tf_code})
-
-def load_json(filename):
-    with open(filename, 'r') as f:
-        return json.load(f)
-
-pricing = load_json("pricing.json")
-regions = load_json("regions.json")
+    return templates.TemplateResponse("result.html", {
+        "request": request,
+        "tf_code": tf_code,
+        "est_cost": est_cost,
+        "default_region": default_region
+    })
